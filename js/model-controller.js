63a834262d38036cb4a00956bdf55b846449c8fc
Alisa Chen <alisa.ky.chen@gmail.com>
2013-08-12 18:07:54 -0400

    Update model-controller.js

--- js/model-controller.js
+++ js/model-controller.js
@@ -1,3 +1,28 @@
+/* PhoneGap Ready
+ * 
+ * using an AngularJS promise to run script when PhoneGap is ready
+ *
+ * author: Andy Joslin (ajoslin), iterating on Brian Ford (bford)'s wrapper
+ * source: https://github.com/btford/angular-phonegap-ready/issues/1
+ */
+
+angular.module('btford.phonegap.ready', []).
+  factory('phonegapReady', function ($rootScope, $q) {
+    var loadingDeferred = $q.defer();
+    document.addEventListener('deviceready', function() {
+      $rootScope.$apply(loadingDeferred.resolve);
+    });
+    return function phonegapReady() {
+      return loadingDeferred.promise;
+    };
+  });
+  
+
+// NOTE: use like this
+// phonegapReady().then(function() {
+//   doStuff();
+// });
+
 phonegapReady().then(function() {
   function TodoCtrl($scope) {
     $scope.todos = [
