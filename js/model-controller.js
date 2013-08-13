cdce17fd3fcee33790f04fd503b1df315f3013f3
Alisa Chen <alisa.ky.chen@gmail.com>
2013-08-13 10:17:45 -0400

    Update model-controller.js

--- js/model-controller.js
+++ js/model-controller.js
@@ -1,11 +1,3 @@
-/* PhoneGap Ready
- * 
- * using an AngularJS promise to run script when PhoneGap is ready
- *
- * author: Andy Joslin (ajoslin), iterating on Brian Ford (bford)'s wrapper
- * source: https://github.com/btford/angular-phonegap-ready/issues/1
- */
-
 angular.module('btford.phonegap.ready', []).
   factory('phonegapReady', function ($rootScope, $q) {
     var loadingDeferred = $q.defer();
@@ -16,38 +8,15 @@ angular.module('btford.phonegap.ready', []).
       return loadingDeferred.promise;
     };
   });
-  
-
-// NOTE: use like this
-// phonegapReady().then(function() {
-//   doStuff();
-// });
 
 phonegapReady().then(function() {
-  function TodoCtrl($scope) {
-    $scope.todos = [
-      {text:'learn angular', done:true},
-      {text:'build an angular app', done:false}];
-   
-    $scope.addTodo = function() {
-      $scope.todos.push({text:$scope.todoText, done:false});
-      $scope.todoText = '';
-    };
-   
-    $scope.remaining = function() {
-      var count = 0;
-      angular.forEach($scope.todos, function(todo) {
-        count += todo.done ? 0 : 1;
-      });
-      return count;
-    };
-   
-    $scope.archive = function() {
-      var oldTodos = $scope.todos;
-      $scope.todos = [];
-      angular.forEach(oldTodos, function(todo) {
-        if (!todo.done) $scope.todos.push(todo);
-      });
-    };
-  }
+  var app = angular.module('field-inspection-app', ['ngGrid']);
+  app.controller('MyCtrl', function($scope) {
+      $scope.myData = [{name: "Moroni", age: 50},
+                       {name: "Tiancum", age: 43},
+                       {name: "Jacob", age: 27},
+                       {name: "Nephi", age: 29},
+                       {name: "Enos", age: 34}];
+      $scope.gridOptions = { data: 'myData' };
+  });
 });
